31731239c64d68d3d73dca99778cd42d3423c526
fancyabc <2310403052@qq.com>
2022-06-30 17:47:57 +0800

    表单的基本使用

--- hello.py
+++ hello.py
@@ -1,18 +1,37 @@
 # from ensurepip import bootstrap
+from crypt import methods
 from datetime import datetime
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+'''app.config 字典可用于存储 Flask、扩展和应用自身的配置变量。
+使用标准的字典句法就能把配置添加到 app.config 对象中。
+这个对象还提供了一些方法，可以从文件或环境中导入配置。'''
+app.config['SECRET_KEY'] = 'hard to guess string' # 配置 Flask-WTF
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)  # 初始化 Flask-Moment
 
-@app.route('/')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                            current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',form=form,name=name)
 
 
 @app.route('/user/<name>')
