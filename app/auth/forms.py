37fd032114a84145f1661f56e7c182a9d90c0afd
fancyabc <2310403052@qq.com>
2022-07-01 18:37:02 +0800

    基本的注册功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+
+from ..models import User
 
 """
-Note: 2.3.0以后的WTForms已经不支持email验证器了，而此处用来建立表单的flask-wtf是集成了wtforms的，所以也是没有该验证器的了。
+Note: 2.3.0以后的WTForms已经不直接支持Email()验证器了，而此处用来建立表单的flask-wtf是集成了wtforms的，所以也是没有该验证器的了。
 需要再自行下载包
  pipenv install email-validator
 """
@@ -12,3 +15,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1,64),Email()])
+    username = StringField('Username', validators=[
+                            DataRequired(), Length(1, 64),
+                            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                            'Usernames must have only letters, numbers, dots or '
+                            'underscores')]) 
+    password = PasswordField('Password', validators=[DataRequired(),
+                            EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Comfirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered!')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('username already in use!')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,17 @@
+from crypt import methods
+import email
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
@@ -27,3 +30,17 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can login now.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
