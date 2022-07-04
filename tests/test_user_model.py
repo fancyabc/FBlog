042a83c1a9efb9199d64bbd736fed6cfde2469cb
fancyabc <2310403052@qq.com>
2022-07-04 19:11:33 +0800

    角色和权限的单元测试和错误页面

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,7 @@
 '''密码散列测试'''
 
 import unittest
-from app.models import User
+from app.models import User, Permission,AnonymousUser
 
 # 每次想确认一切功能是否正常时，可以运行单元测试组建
 
@@ -24,3 +24,19 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_user_role(self):         
+        u = User(email='john@example.com', password='cat')         
+        self.assertTrue(u.can(Permission.FOLLOW)) 
+        self.assertTrue(u.can(Permission.COMMENT))         
+        self.assertTrue(u.can(Permission.WRITE))         
+        self.assertFalse(u.can(Permission.MODERATE))         
+        self.assertFalse(u.can(Permission.ADMIN))      
+        
+    def test_anonymous_user(self):         
+        u = AnonymousUser()         
+        self.assertFalse(u.can(Permission.FOLLOW))         
+        self.assertFalse(u.can(Permission.COMMENT))         
+        self.assertFalse(u.can(Permission.WRITE))         
+        self.assertFalse(u.can(Permission.MODERATE))         
+        self.assertFalse(u.can(Permission.ADMIN))
