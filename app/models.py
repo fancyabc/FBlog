8ab0ff80ce0aaf0523720dcfb3d1cadf3c7ddb56
fancyabc <2310403052@qq.com>
2022-07-11 21:59:39 +0800

    在服务器端处理富文本

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from authlib.jose import jwt, JoseError # 生成用于验证的token
 from flask import current_app
 from flask_avatars import Identicon
+from markdown import markdown
+import bleach
 
 from . import db
 from . import login_manager
@@ -80,9 +82,23 @@ class Role(db.Model):
 class Post(db.Model):     
     __tablename__ = 'posts'     
     id = db.Column(db.Integer, primary_key=True)     
-    body = db.Column(db.Text)     
+    body = db.Column(db.Text)   
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)     
     author_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+
+    '''在Post模型中处理Markdown文本'''
+    @staticmethod     
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
         
 class User(UserMixin, db.Model):     # 修改 User 模型，支持用户登录
