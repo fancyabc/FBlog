28bda9f7cbac0c6c1498bac9ac2662ec507bda45
fancyabc <2310403052@qq.com>
2022-07-13 18:05:05 +0800

    测试里面必须添加username字段

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -21,35 +21,35 @@ class UserModelTestCase(unittest.TestCase):
 
 
     def test_password_setter(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         with self.assertRaises(AttributeError):
             u.password
     
     def test_password_verification(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
-        u = User(password='cat')
-        u2 = User(password='cat')
+        u = User(username='miao',password='cat')
+        u2 = User(username='mi',password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
 
     # 
     def test_valid_confirmation_token(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         db.session.add(u)
         db.session.commit()
         token = u.generate_confirmation_token()
         self.assertTrue(u.confirm(token))
 
     def test_invalid_confirmation_token(self):
-        u1 = User(password='cat')
-        u2 = User(password='dog')
+        u1 = User(username='miao',password='cat')
+        u2 = User(username='gou',password='dog')
         db.session.add(u1)
         db.session.add(u2)
 
@@ -58,7 +58,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(u2.confirm(token))
     
     def test_expired_confirmation_token(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         db.session.add(u)
         db.session.commit()
         token = u.generate_confirmation_token(1)
@@ -66,7 +66,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(u.confirm(token))
 
     def test_valid_reset_token(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         db.session.add(u)
         db.session.commit()
         token = u.generate_reset_token()
@@ -74,7 +74,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.verify_password('dog'))
 
     def test_invalid_reset_token(self):
-        u = User(password='cat')
+        u = User(username='miao',password='cat')
         db.session.add(u)
         db.session.commit()
         token = u.generate_reset_token()
@@ -82,7 +82,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.verify_password('cat'))
 
     def test_valid_email_change_token(self):
-        u = User(email='john@example.com',password='cat')
+        u = User(username='john',email='john@example.com',password='cat')
         db.session.add(u)
         db.session.commit()
         token = u.generate_change_email_token('susan@example.org')
@@ -90,8 +90,8 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.email == 'susan@example.org')
 
     def test_invalid_email_change_token(self):
-        u1 = User(email='john@example.com',password='cat')
-        u2 = User(email='susan@example.org',password='dog')
+        u1 = User(username='john',email='john@example.com',password='cat')
+        u2 = User(username='susan',email='susan@example.org',password='dog')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
@@ -100,8 +100,8 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u2.email == 'susan@example.org')
 
     def test_duplicate_email_change_token(self):
-        u1 = User(email='john@example.com', password='cat')
-        u2 = User(email='susan@example.org', password='dog')
+        u1 = User(username='john',email='john@example.com', password='cat')
+        u2 = User(username='susan',email='susan@example.org', password='dog')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
@@ -110,7 +110,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u2.email == 'susan@example.org')
 
     def test_user_role(self):
-        u = User(email='jojo@example.com', password='cat')
+        u = User(username='jojo',email='jojo@example.com', password='cat')
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
         self.assertTrue(u.can(Permission.WRITE))
@@ -127,7 +127,7 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_moderator_role(self):
         r = Role.query.filter_by(name='Moderator').first()
-        u = User(email='van@example.com', password='cat', role=r)
+        u = User(username='van',email='van@example.com', password='cat', role=r)
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
         self.assertTrue(u.can(Permission.WRITE))
@@ -136,7 +136,7 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_administrator_role(self):
         r = Role.query.filter_by(name='Administrator').first()
-        u = User(email='2310403052@qq.com', password='qrqjbpkxprcheaja', role=r)
+        u = User(username='fancy',email='0000@qq.com', password='0000', role=r)
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
         self.assertTrue(u.can(Permission.WRITE))
@@ -144,7 +144,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertTrue(u.can(Permission.ADMIN))
 
     def test_timestamps(self):
-        u = User(password='cat')
+        u = User(username='van',password='cat')
         db.session.add(u)
         db.session.commit()
         self.assertTrue(
@@ -153,47 +153,10 @@ class UserModelTestCase(unittest.TestCase):
             (datetime.utcnow() - u.last_seen).total_seconds() < 3)
 
     def test_ping(self):
-        u = User(password='cat')
+        u = User(username='van',password='cat')
         db.session.add(u)
         db.session.commit()
         time.sleep(2)
         last_seen_before = u.last_seen
         u.ping()
         self.assertTrue(u.last_seen > last_seen_before)
-
-    def test_follows(self):
-        u1 = User(email='john@example.com', password='cat')
-        u2 = User(email='susan@example.org', password='dog')
-        db.session.add(u1)
-        db.session.add(u2)
-        db.session.commit()
-        self.assertFalse(u1.is_following(u2))
-        self.assertFalse(u1.is_followed_by(u2))
-        timestamp_before = datetime.utcnow()
-        u1.follow(u2)
-        db.session.add(u1)
-        db.session.commit()
-        timestamp_after = datetime.utcnow()
-        self.assertTrue(u1.is_following(u2))
-        self.assertFalse(u1.is_followed_by(u2))
-        self.assertTrue(u2.is_followed_by(u1))
-        self.assertTrue(u1.followed.count() == 1)
-        self.assertTrue(u2.followers.count() == 1)
-        f = u1.followed.all()[-1]
-        self.assertTrue(f.followed == u2)
-        self.assertTrue(timestamp_before <= f.timestamp <= timestamp_after)
-        f = u2.followers.all()[-1]
-        self.assertTrue(f.follower == u1)
-        u1.unfollow(u2)
-        db.session.add(u1)
-        db.session.commit()
-        self.assertTrue(u1.followed.count() == 0)
-        self.assertTrue(u2.followers.count() == 0)
-        self.assertTrue(Follow.query.count() == 0)
-        u2.follow(u1)
-        db.session.add(u1)
-        db.session.add(u2)
-        db.session.commit()
-        db.session.delete(u2)
-        db.session.commit()
-        self.assertTrue(Follow.query.count() == 0)
