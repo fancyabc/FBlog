4b50612f5520ca6cdf2c21ed9b4988f7e1346260
fancyabc <2310403052@qq.com>
2022-07-14 15:57:32 +0800

    使用coverage获取代码覆盖度报告

--- fblog.py
+++ fblog.py
@@ -1,4 +1,12 @@
 import os
+import sys
+import click
+
+COV = None 
+if os.environ.get('FBLOG_COVERAGE'):     
+    import coverage     
+    COV = coverage.coverage(branch=True, include='app/*')     
+    COV.start()
 
 from app import create_app, db
 from app.models import User, Role, Follow, Permission, Post, Comment
@@ -16,9 +24,25 @@ def make_shell_context():
         Follow=Follow, Post=Post, Comment=Comment)
 
 
-@app.cli.command() 
-def test():     
-    """Run the unit tests."""     
+@app.cli.command()
+@click.option('--coverage/--no-coverage', default=False,help='Run tests under code coverage.')
+def test(coverage):     
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):         
+        os.environ['FLASK_COVERAGE'] = '1'         
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+
     import unittest     
     tests = unittest.TestLoader().discover('tests')     
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:         
+        COV.stop()         
+        COV.save()         
+        print('Coverage Summary:')         
+        COV.report()         
+        basedir = os.path.abspath(os.path.dirname(__file__))         
+        covdir = os.path.join(basedir, 'tmp/coverage')         
+        COV.html_report(directory=covdir)         
+        print('HTML version: file://%s/index.html' % covdir)         
+        COV.erase()
